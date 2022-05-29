e659392d21fbb2bbf2dc873dc27a827967c9b070
Lamtev Artem <lamtev2000@gmail.com>
2022-05-29 21:15:11 +0300

    add modal when game is over

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,45 @@
-import React, { FC, useState } from 'react';
-import { useActoins } from '../hooks/useActions';
-import { useTypedSelector } from '../hooks/usedTypeSelector';
-import { cell } from '../types/board';
-import Cell from './Cell';
+import React, { FC, useState } from "react";
+import { useActoins } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/usedTypeSelector";
+import { cell, GameStatus } from "../types/board";
+import Cell from "./Cell";
 
-const Board:FC = () => {
-  const board = useTypedSelector<cell[]>(store => store.board.board)
-  const turn = useTypedSelector<string>(store => store.board.turn)
-  const {clickOnFigure, movePiece} = useActoins()
-  const [active, setActive] = useState<cell | null>(null)
-  const [formerCell, setFormerCell] = useState<cell | null>(null)
-  const arr = ["a", "c", "e", "g"]
-  function handleClick(cell:cell){
-    setActive(cell)
-    if(cell.available === true)
-      movePiece(cell, board, formerCell!)
-    else
-      clickOnFigure(cell, board, turn)
-    setFormerCell(cell)
+const Board: FC = () => {
+  const { board, end } = useTypedSelector((store) => store.board);
+  const turn = useTypedSelector<"white" | "dark">((store) => store.board.turn);
+  const { clickOnFigure, movePiece, restart } = useActoins();
+  const [active, setActive] = useState<cell | null>(null);
+  const [formerCell, setFormerCell] = useState<cell | null>(null);
+  const arr = ["a", "c", "e", "g"];
+  function handleClick(cell: cell) {
+    setActive(cell);
+    if (cell.available === true) movePiece(cell, board, formerCell!);
+    else clickOnFigure(cell, board, turn);
+    setFormerCell(cell);
   }
   return (
     <div className="flex board">
-      {board.map((cell:cell)=>
-          <Cell
-            key={cell.cell}
-            cell={cell}
-            active = {active}
-            onClick={handleClick}
-            color={
-              (arr.indexOf(cell.cell[0]) === -1 &&  (+cell.cell[1] % 2 === 0))
-              || (arr.indexOf(cell.cell[0]) !== -1 &&  (+cell.cell[1] % 2 !== 0))
-              ? "black" : "white"}/>
-      )}
+      {board.map((cell: cell) => (
+        <Cell
+          key={cell.cell}
+          cell={cell}
+          active={active}
+          onClick={handleClick}
+          color={
+            (arr.indexOf(cell.cell[0]) === -1 && +cell.cell[1] % 2 === 0) ||
+            (arr.indexOf(cell.cell[0]) !== -1 && +cell.cell[1] % 2 !== 0)
+              ? "black"
+              : "white"
+          }
+        />
+      ))}
+      {end !== GameStatus.PLAYING ? (
+        <div className="board__modal modal">
+          {end}
+          <br />
+          <button onClick={restart}>Начать снова</button>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,24 +1,26 @@
-import React, { FC } from 'react';
-import { cell } from '../types/board';
-interface CellProps{ 
-  cell: cell,
-  color: string,
-  active: cell | null,
-  onClick: (cell:cell) => void
+import React, { FC } from "react";
+import { cell } from "../types/board";
+interface CellProps {
+  cell: cell;
+  color: string;
+  active: cell | null;
+  onClick: (cell: cell) => void;
 }
-const Cell:FC<CellProps> = ({cell, color, active, onClick}) => {  
-  const img = cell.piece === null ? '' : require(`../pieces/${cell.piece}.png`);  
-  return(
-    <div className={
-      (color ==="white" ? "cell-white " : "cell-dark " ) + 
-      (cell.available && img !== '' ? "attacked" : '') + 
-      (active?.cell === cell.cell ? " active" : "")}  
+const Cell: FC<CellProps> = ({ cell, color, active, onClick }) => {
+  const img = cell.piece === null ? "" : require(`../pieces/${cell.piece}.png`);
+  return (
+    <div
+      className={
+        (color === "white" ? "cell-white " : "cell-dark ") +
+        (cell.available && img !== "" ? "attacked" : "") +
+        (active?.cell === cell.cell ? " active" : "")
+      }
       onClick={() => onClick(cell)}
     >
-      {cell.available && img === '' ? <div className="cell-available"/> : null}
-      {img === '' ? null : <img draggable src={img} alt={cell.cell}/>}
-    </div>          
-  )
+      {cell.available && img === "" ? <div className="cell-available" /> : null}
+      {img === "" ? null : <img draggable src={img} alt={cell.cell} />}
+    </div>
+  );
 };
 
 export default Cell;
